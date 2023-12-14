0a1f4d7e9ea1c9ee38faff4f1f9c938283eba161
Sparty27 <nyshchyi.nazar@gmail.com>
2023-12-14 19:34:39 +0200

    INCLUDED GULP. Added circle bar animation. Added bundle.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,55 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
- 
-gulp.task('default', function () {
-  return gulp.src('assets/**/*.css')
-    .pipe(concatCss("styles/bundle.css"))
-    .pipe(gulp.dest('out/'));
-});
+const {src, dest, watch, parallel} = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+
+
+function scripts() {
+    const jsBundle = () => {
+      src([
+        'node_modules/jquery-circle-progress/dist/circle-progress.min.js',
+      ])
+      .pipe(concat('circle-progress.min.js'))
+      .pipe(dest('dist/js'));
+    }
+    jsBundle();
+    return src([
+        'src/js/*.js',
+        '!dist/js/main.min.js'
+    ])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(dest('dist/js'))
+        .pipe(browserSync.stream())
+}
+
+function styles() {
+    return src('src/scss/main.scss')
+        .pipe(concat('main.min.css'))
+        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.stream())
+}
+
+function watching() {
+    watch(['src/scss/main.scss'], styles)
+    watch(['src/js/main.js'], scripts)
+    watch(['src/*.html']).on('change', browserSync.reload);
+}
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(styles, scripts, browsersync, watching);
---
+++ src/js/add-circles.js
@@ -0,0 +1,52 @@
+$('#circle').circleProgress({
+value: 0.92,
+startAngle: 2,
+thickness: 32,
+size: 220,
+reverse: true,
+lineCap: "round",
+emptyFill: "rgba(11,11,11,0)",
+fill: {
+gradient: ["#FA709A", "#FEE140"]
+}
+
+});
+$('#circle2').circleProgress({
+value: 0.92,
+startAngle: 2,
+thickness: 32,
+size: 220,
+reverse: true,
+lineCap: "round",
+emptyFill: "rgba(11,11,11,0)",
+fill: {
+gradient: ["#FA709A", "#FEE140"]
+}
+
+});
+$('#circle3').circleProgress({
+value: 0.92,
+startAngle: 2,
+thickness: 32,
+size: 220,
+reverse: true,
+lineCap: "round",
+emptyFill: "rgba(11,11,11,0)",
+fill: {
+gradient: ["#FA709A", "#FEE140"]
+}
+
+});
+$('#circle4').circleProgress({
+value: 0.92,
+startAngle: 2,
+thickness: 32,
+size: 220,
+reverse: true,
+lineCap: "round",
+emptyFill: "rgba(11,11,11,0)",
+fill: {
+gradient: ["#FA709A", "#FEE140"]
+}
+
+});
