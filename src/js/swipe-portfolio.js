598d1b41139430fa0877e2bfcab3f1a7dac6e9fb
Sparty27 <nyshchyi.nazar@gmail.com>
2023-12-26 14:54:30 +0200

    Breakpoints in swiper

--- src/js/swipe-portfolio.js
+++ src/js/swipe-portfolio.js
@@ -3,6 +3,20 @@ let swiper = new Swiper('.swiper', {
     loop: false,
     spaceBetween: 30,
 
+    breakpoints: {
+        1024: {
+            spaceBetween: 30,
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        
+        1538: {
+            spaceBetween: 30,
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+        }
+    },
+
     pagination: {
         el: '.swiper-pagination',
     },
